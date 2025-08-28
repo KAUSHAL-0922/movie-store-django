404062cd771b5ea332831bc08c9cf67b14ad4bf0
KAUSHAL-0922 <sakhreliyakaushal@gmail.com>
2025-08-28 15:09:06 +0530

    Configure production

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -21,15 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ofy(-s41ct*q3y^pz74og0f1qw2pdat!&-29y0d=wg)n^l69j='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ofy(-s41ct*q3y^pz74og0f1qw2pdat!&-29y0d=wg)n^l69j=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
-ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 INSTALLED_APPS = [
